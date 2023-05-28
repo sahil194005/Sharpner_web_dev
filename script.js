208b775107550b3652072e2468ea574d8d867fc7
iamdevopstrainer <sahilkumar2275@gmail.com>
2023-05-28 16:30:36 +0530

    Lets get started with DOM

---
+++ script.js
@@ -0,0 +1,25 @@
+// let hd = document.querySelector('head')
+
+// let heading = document.createElement('h1')
+// heading.appendChild(document.createTextNode('Title'))
+// hd.appendChild(heading)
+
+// let form = document.querySelector('.form-inline.mb-3')
+// // console.log(form)
+// let newfield = document.createElement('input')
+// newfield.type = "text"
+// newfield.name = "Name"
+// newfield.classList = "form-control mr-2"
+
+// form.appendChild
+// (newfield)
+// *******************************************************************************
+// let title = (document.getElementById('header-title'))
+// console.log(title)
+// title.innerHTML = '<h2>puts inside h1 </h2>'
+let heading  = document.querySelector('.title')
+heading.style.color = 'green'
+heading.style.fontWeight = 'bold'
+
+let title = document.querySelector('.container')
+title.style.border = '10px solid black'
