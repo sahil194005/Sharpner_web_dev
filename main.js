fd6e3accf0f04af9902e2b3cfa9f154f983103a3
iamdevopstrainer <sahilkumar2275@gmail.com>
2023-05-29 17:52:50 +0530

    Lets Scale the app to more users

--- main.js
+++ main.js
@@ -1,17 +1,60 @@
-let submit= document.querySelector('.btn')
- 
-submit.addEventListener('click',onclick)
+let nameInput = document.querySelector("#name-input");
+let emailInput = document.querySelector("#email-input");
+let phoneInput = document.querySelector("#phoneNo-input");
 
-function onclick(e){
-  e.preventDefault()
-  let name = document.querySelector('#name').value
-  let email = document.querySelector('#email').value
+let submit = document.querySelector("#submit");
+submit.addEventListener("click", onsubmit);
+
+function onsubmit(e) {
+  e.preventDefault();
+  let name_val = nameInput.value;
+  let email_val = emailInput.value;
+  let phone_val = phoneInput.value;
   let myobj = {
-    firstName : name,
-    PersonalEmail: email
+    name: name_val,
+    email: email_val,
+    phoneNo: phone_val,
+  };
+
+  if (localStorage.getItem("details")) {
+    let details = JSON.parse(localStorage.getItem("details"));
+    let myArray = details;
+
+    myArray.push(myobj);
+
+    myobj_string = JSON.stringify(myArray);
+    localStorage.setItem("details", myobj_string);
+  } else {
+    let myArray = [];
+    myArray.push(myobj);
+
+    myobj_string = JSON.stringify(myArray);
+    localStorage.setItem("details", myobj_string);
   }
-  let myobj_string = JSON.stringify(myobj)
-  localStorage.setItem("user1",myobj_string)
-  let myobj_json = JSON.parse(myobj_string)
-  console.log(myobj_json)
+
+  nameInput.value = "";
+  emailInput.value = "";
+  phoneInput.value = "";
+
+  // Display the details in an unordered list
+  displayDetails();
+}
+
+function displayDetails() {
+  let detailsContainer = document.getElementById("details-container");
+  detailsContainer.innerHTML = ""; // Clear the container before adding new details
+
+  let details = JSON.parse(localStorage.getItem("details"));
+
+  if (details && details.length > 0) {
+    let ul = document.createElement("ul");
+
+    details.forEach((detail) => {
+      let li = document.createElement("li");
+      li.textContent = `Name: ${detail.name}, Email: ${detail.email}, Phone: ${detail.phoneNo}`;
+      ul.appendChild(li);
+    });
+
+    detailsContainer.appendChild(ul);
+  }
 }
