598e6d3567608e0eab54e85780f2d0b50e568cac
iamdevopstrainer <sahilkumar2275@gmail.com>
2023-05-29 16:14:46 +0530

    Lets store the users in the right format

--- main.js
+++ main.js
@@ -6,5 +6,12 @@ function onclick(e){
   e.preventDefault()
   let name = document.querySelector('#name').value
   let email = document.querySelector('#email').value
-  localStorage.setItem(name,email)
+  let myobj = {
+    firstName : name,
+    PersonalEmail: email
+  }
+  let myobj_string = JSON.stringify(myobj)
+  localStorage.setItem("user1",myobj_string)
+  let myobj_json = JSON.parse(myobj_string)
+  console.log(myobj_json)
 }
