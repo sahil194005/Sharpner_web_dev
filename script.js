c0e6a0a86c65b320f372654f66cea7da83db7503
iamdevopstrainer <sahilkumar2275@gmail.com>
2023-05-29 12:04:15 +0530

    Filter

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 let form = document.querySelector("#addForm")
 let itemlist = document.querySelector("#items")
+let filter = document.querySelector('#filter')
 
 form.addEventListener("submit", onsubmit)
 itemlist.addEventListener('click',removeitem)
+filter.addEventListener('keyup',matchingfunc)
+
+
+
+
+
+function matchingfunc(e){
+    e.preventDefault()
+    var text = e.target.value.toLowerCase()
+  var items =itemlist.getElementsByTagName('li')
+   Array.from(items).forEach(function (item) {
+    var itemname = item.firstChild.textcontent
+    if(itemname.toLowerCase().indexOf(text)!=-1){
+        item.style.display = 'block'
+    }
+    else{
+        item.style.display = 'none'
+    }
+   })
+   
+}
+
+
+
 
 
 function removeitem(e){
@@ -35,25 +60,24 @@ function onsubmit(e) {
 
 
 
-let item = document.querySelectorAll('.list-group-item')
 
 
 
 
 
 
-//adding the edit button
-var listItems = document.querySelectorAll('#items li');
+    //adding the edit button
+    // var listItems = document.querySelectorAll('#items li');
 
-listItems.forEach(function(item) {
-  var editButton = document.createElement('button');
-  editButton.classList.add('btn', 'btn-primary', 'btn-sm', 'float-right');
-  editButton.textContent = 'Edit';
+    // listItems.forEach(function(item) {
+    //   var editButton = document.createElement('button');
+    //   editButton.classList.add('btn', 'btn-primary', 'btn-sm', 'float-right');
+    //   editButton.textContent = 'Edit';
 
-  var deleteButton = item.querySelector('.delete');
+    //   var deleteButton = item.querySelector('.delete');
 
-  
-  deleteButton.insertAdjacentElement('afterend', editButton);
-});
+    
+    //   deleteButton.insertAdjacentElement('afterend', editButton);
+    // });
 
 
